fcf0d031374e8a7f989ebe0cce08bc02617b3b16
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2016-09-27 18:47:01 +0200

    add os x and linux specific instructions and caveats for licensing terms.

--- soplex.rb
+++ soplex.rb
@@ -7,16 +7,32 @@ class Soplex < Formula
   option "without-test", "Skip build-time tests (not recommended)"
 
   depends_on "gcc"
-  depends_on "zlib"
+  depends_on "zlib" if OS.linux?
   depends_on "gmp"
 
   def install
-    system "make", "SHARED=true"
+    if OS.mac?
+      File.open("make/make.darwin.x86_64.gnu", "a") do |f|
+        f.puts "LIBBUILDFLAGS+= -m64 -L/usr/local/lib -lgmp -lz"
+      end
+    end
+    system "make", "SHARED=true", "USRCXXFLAGS=-I/usr/local/include", "USRLDFLAGS=-L/usr/local/lib"
     system "make", "test" if build.with? "test"
     lib.install Dir["lib/libsoplex*"]
     bin.install Dir["bin/soplex*"]
     include.install Dir["src/*.h"]
     pkgshare.install "src/example.cpp"
+  end
+
+  def caveats; <<-EOS.undent
+      SoPlex is distributed under the ZIB Academic License
+      (http://scip.zib.de/academic.txt).
+      You are allowed to retrieve SoPlex for research purposes as a member of
+      a non-commercial and academic institution.
+
+      We agreed to this license for you.
+      If this is unacceptable you should uninstall.
+    EOS
   end
 
   test do
